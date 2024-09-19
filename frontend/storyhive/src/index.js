5321dd5dc8cfb0c0f6a02074e6ae5ffff9746af4
nthn22 <nathanzamora45@gmail.com>
2024-09-18 19:54:28 -0700

    imported buttons for table contents: user - userstats

--- frontend/storyhive/src/index.js
+++ frontend/storyhive/src/index.js
@@ -44,22 +44,328 @@ root.render(
     </button>
 
     <p id="user">User contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#user");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=user",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      User
+    </button>
 
     <p id="user-profile">UserProfile contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#user-profile");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=user-profile",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      UserProfile
+    </button>
 
     <p id="user-settings">UserSettings contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#user-settings");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=user-settings",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      UserSettings
+    </button>
 
     <p id="location">Location contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#location");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=location",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      Location
+    </button>
 
     <p id="user-status">UserStatus contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#user-status");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=user-status",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      UserStatus
+    </button>
 
     <p id="following-status">FollowingStatus contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#following-status");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=following-status",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      FollowingStatus
+    </button>
 
     <p id="friend-status">FriendStatus contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#friend-status");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=friend-status",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      FriendStatus
+    </button>
 
     <p id="block-status">BlockStatus contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#block-status");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=block-status",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      BlockStatus
+    </button>
 
     <p id="user-stats">UserStats contents: </p>
+    <button
+      onClick={() => {
+        // Stores the text from the <p> element for posts
+        const text = document.querySelector("#user-stats");
+
+        // Change the text in the <p> when the button is pressed
+        text.textContent = "waiting...";
+
+        // Make a GET request to the '/posts' endpoint
+        fetch(
+          "https://storyhive-app.onrender.com/fetch-data?collection=user-stats",
+          { method: "GET" }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Process the response from the API
+            var data_obj = JSON.stringify(data);
+            data_obj = JSON.parse(data_obj);
+
+            // Pretty printing the data
+            var output = "| ";
+            for (let key in data_obj[0]) {
+              if (data_obj[0].hasOwnProperty(key)) {
+                let value = data_obj[0][key];
+                output += key + " : " + value + " | ";
+              }
+            }
+            // Change the text in the <p> element to display the data
+            text.textContent = output;
+          });
+      }}
+    >
+      UserStats
+    </button>
 
     <p id="admin">Admin contents: </p>
     <button
