51be6e1657b1ba0584ea589e06ea1006cea78de6
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-10-17 02:51:05 -0700

    hexagon friends frame

--- frontend/storyhive/src/components/FriendsList.js
+++ frontend/storyhive/src/components/FriendsList.js
@@ -1,5 +1,9 @@
 //component FriendsList
 import React from "react";
+import "./hexagon.css";
+import beeLogo from "./bee.png";
+import "./flower.png";
+
 
 export default function FriendsList({ friends }) {
   return (
@@ -9,11 +13,56 @@ export default function FriendsList({ friends }) {
         {friends.length > 0 ? (
           friends.map((friend, index) => (
             <li key={index} className="bg-[#eec33d] p-2 rounded-md">
+              
               {friend} {/* Display each friend's username or friendID */}
             </li>
           ))
         ) : (
-          <li className="text-gray-400">No friends to display.</li>
+          <ul>
+            <li className="text-gray-400">No friends to display.</li>
+            <div class="container">
+              <div class="hexagonArea">
+                <div class="hexagon">
+                  <img src="bee.png" alt=""/>
+                  <h3>hexagon1</h3>
+                </div>
+                
+                <div class="hexagon">
+                  <img src="flower.png" alt=""/>
+                  <h3>hexagon2</h3>
+                </div>
+                
+                <div class="hexagon">
+                  <img src="bee.png" alt=""/>
+                  <h3>hexagon3</h3>
+                </div>
+
+                <div class="hexagon">
+                  <img src="flower.png" alt=""/>
+                  <h3>hexagon4</h3>
+                </div>
+
+              </div>
+              <div class="hexagonArea">
+                <div class="hexagon">
+                  <img src="bee.png" alt=""/>
+                  <h3>hexagon5</h3>
+                </div>
+
+                <div class="hexagon">
+                  <img src="flower.png" alt=""/>
+                  <h3>hexagon6</h3>
+                </div>
+
+                <div class="hexagon">
+                  <img src="bee.png" alt=""/>
+                  <h3>hexagon7</h3>
+                </div>
+
+              </div>
+            </div>
+        </ul>
+          
         )}
       </ul>
     </div>
