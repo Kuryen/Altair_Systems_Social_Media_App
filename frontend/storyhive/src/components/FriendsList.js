9eba0c2e605de47cc14f11132b5cf802f449afaf
JonathanRR92 <91393278+JonathanRR92@users.noreply.github.com>
2024-10-20 01:04:06 -0700

    update
    frinedslist

--- frontend/storyhive/src/components/FriendsList.js
+++ frontend/storyhive/src/components/FriendsList.js
@@ -1,64 +1,26 @@
-//component FriendsList
+// component FriendsList
 import React from "react";
 import "./hexagon.css";
-import beeLogo from "./pics/bee.png";
-import flower from"./pics/flower.png";
-import guy from "./pics/guystockphoto.jpg";
-import pic from "./pics/profilepic.png";
-
 
 export default function FriendsList({ friends }) {
+
+  
   return (
     <div className="bg-black p-4 text-white w-full h-full">
       <h3 className="text-xl font-bold mb-4">Friends List</h3>
-      <ul className="space-y-2">
-        {friends.length > 0 ? (
-          friends.map((friend, index) => (
-            <li key={index} className="bg-[#eec33d] p-2 rounded-md">
-              
-              {friend} {/* Display each friend's username or friendID */}
-            </li>
-          ))
-        ) : (
-          <ul>
-            <li className="text-gray-400">No friends to display.</li>
-            <div class="container">
-              <div class="hexagonArea">
-                <div class="hexagon">
-                  {/* <img src={guy} alt="" class="w-full h-full"/> */}
-                  <h6>Username</h6>
-                </div>
-                
-                <div class="hexagon">
-                  <img src={guy} alt="" class="w-full h-full"/>
-                </div>
-                
-                <div class="hexagon">
-                  <img src={pic} alt="" class="w-full h-full"/>
-                </div>
-
-                <div class="hexagon">
-                  <img src={flower} alt="" class="w-full h-full"/>
-                </div>
-
-                <div class="hexagon">
-                  <img src={beeLogo} alt="" class="w-full h-full"/>
-                </div>
-
-                <div class="hexagon">
-                  <img src={flower} alt="" class="w-full h-full"/>
-                </div>
-
-                <div class="hexagon">
-                  <img src={beeLogo} alt="" class="w-full h-full"/>
-                </div>
-
+      <div className="container">
+        <div className="hexagonArea">
+          {friends.length > 0 ? (
+            friends.map((friend, index) => (
+              <div key={index} className="hexagon">
+                <h6>{friend}</h6> {/* Display each friend's name in hexagon */}
               </div>
-            </div>
-        </ul>
-          
-        )}
-      </ul>
+            ))
+          ) : (
+            <div className="text-gray-400">No friends to display.</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
