86a9656bb5d50b459b52aabeb53cf1513511d2e6
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-11-08 20:44:11 -0800

    post test

--- frontend/storyhive/testing/post_tests.js
+++ frontend/storyhive/testing/post_tests.js
@@ -3,24 +3,76 @@ function run_post_tests(){
     post_test();
 }
 
-const userPostsEmpty = [];
-const userPosts = ["hello"];
-const arrayTest = {
-    a: 'somestring',
-    b: 42,
-    c: false,
-  };
+// const user = "matt";
+// const userPostsEmpty = [];
+// const userPosts = ["hello", "world", "!"];
+// const arrayTest = {
+//     a: 'somestring',
+//     b: 42,
+//     c: false,
+//   };
 
-const arrayTestEmpty = {};
+// const arrayTestEmpty = {};
 
-post_test(arrayTest, userPostsEmpty);
+// post_test(arrayTest, userPostsEmpty);
+// post_test(arrayTest, userPosts);
+// post_test(arrayTestEmpty, userPosts);
+// post_test(arrayTestEmpty, userPostsEmpty);
 
-post_test(arrayTest, userPosts);
+function userPosts_test(currentUser)
+{
+    fetch(`https://storyhive-app.onrender.com/posting/posts?user=${currentUser}`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((json) => {
+            // Filter posts by current user's username
+            const userPosts = Object.values(json).filter((post) => {
+                const userName =
+                  typeof post.userID === "object" ? post.userID.name : post.userID;
+                return userName === currentUser;
+              });
+        })
+}
+//////////////////////////////////////////////////////////////////////
+function postDate_test(userPosts)
+{
+            userPosts.forEach((post) => {
+                // User information
+                const userName =
+                  typeof post.userID === "object"
+                    ? post.userID.name
+                    : post.userID || "Unknown User";
+                let createdAt = "Date Unavailable";
+                if (post.createdAt) {
+                  const date = new Date(post.createdAt);
+                  if (!isNaN(date.getTime())) {
+                    createdAt = date.toLocaleString();
+                  }
+                }
+              });
+}
+//////////////////////////////////////////////////////////////////////
+function postContent_test(userPosts){
+    userPosts.forEach((post) => {
+        if (post.textContent)
+        {
+            console.log("post: " + post);
+        }
+    });
+}
+//////////////////////////////////////////////////////////////////////
+function postTrim_test(userPosts){
+    userPosts.forEach((post) => {
+        if (post.media && post.media.trim() !== "")
+        {
+            console.log("post: " + post.textContent.trim());
+        }
+    });
+}
+        
 
-post_test(arrayTestEmpty, userPosts);
-
-post_test(arrayTestEmpty, userPostsEmpty);
-
+//////////////////////////////////////////////////////////////////////
 function post_test(arrTest, posts){
     console.log("-------------post test start-----------");
 
@@ -37,6 +89,13 @@ function post_test(arrTest, posts){
     }
 
     console.log("user has posts")
+
+    posts.forEach((post) => {
+        if (post.textContent)
+        {
+            console.log("post: " + post);
+        }
+    });
 
 }
 
