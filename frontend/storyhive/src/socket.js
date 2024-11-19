d4d54bb3678e3f9d1afdec4a02c467d1141909fb
MattBaldivino <mattbaldivino@gmail.com>
2024-11-18 19:29:43 -0800

    deployment version

--- frontend/storyhive/src/socket.js
+++ frontend/storyhive/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:10000", {
+const socket = io("https://storyhive-app.onrender.com", {
   autoConnect: false,
 }); // Replace with your actual server URL
 export default socket;
