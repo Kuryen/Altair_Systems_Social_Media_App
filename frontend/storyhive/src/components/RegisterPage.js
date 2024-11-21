92ac16aa2ff444fab28f8f8b27b2d35f0fdf669c
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-11-20 16:22:32 -0800

    register page external styling

--- frontend/storyhive/src/components/RegisterPage.js
+++ frontend/storyhive/src/components/RegisterPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import beeLogo from "./pics/bee.png";
+import "../css/loginRegister.css";
 
 function RegisterPage({ onSwitchLoginClick }) {
   function validate(name, password, email) {
@@ -25,34 +26,33 @@ function RegisterPage({ onSwitchLoginClick }) {
     return errors;
   }
   return (
-    <div className="w-[1920px] h-[1239px] relative bg-[#bf6a02] border border-white">
-      <div className="w-[1920px] h-[1239px] left-0 top-0 absolute border border-black">
+    <div className="LRContainer">
         <button
           onClick={onSwitchLoginClick}
-          className="w-[350px] h-[95px] left-[1366px] top-[100px] absolute"
+          className="switchTab"
         >
-          <div className="w-[350px] h-[95px] left-0 top-[1px] absolute bg-[#a2845e] rounded-[100px]"></div>
+          <div className="left-0 top-[1px] selectedTab"></div>
 
-          <div className="w-[198px] h-[95px] left-40 top-0 absolute bg-[#fff1c2] rounded-[100px]"></div>
-          <div className="left-[215px] top-[29px] absolute text-[#111111] text-3xl font-bold font-['Poppins'] lowercase">
+          <div className="left-40 top-0 unselectedTab"></div>
+          <div className="left-[215px] top-[29px] textOfSelectedTab">
             SIGNUP
           </div>
-          <div className="left-[60px] top-[35px] absolute text-[#222222] text-xl font-bold font-['Poppins'] lowercase">
+          <div className="left-[60px] top-[35px] textOfUnselectedTab">
             LOGIN
           </div>
         </button>
 
-        <div className="w-[771px] h-[820px] left-[575px] top-[244px] absolute rounded-[100px] bg-white shadow-xl"></div>
-        <div className="w-[607px] h-[60px] left-[760px] top-[409px] relative pl-7 text-black text-[40px] font-bold font-['Poppins']">
+        <div className="left-[575px] top-[244px] whiteBack"></div>
+        <div className="left-[760px] top-[409px] welcomeText">
           Welcome to Storyhive!
         </div>
 
         <form id="reg-form">
           {/* username bubble and input */}
-          <div className="w-[584px] h-[126px] left-[669px] top-[420px] relative">
+          <div className="left-[669px] top-[420px] inputContainer">
             <label className="w-full h-full" htmlFor="unamer">
               <input
-                className="rounded-[100px] border-2 border-[#aaaaaa] text-black text-2xl w-full h-full pl-10 placeholder:text-2xl placeholder:italic"
+                className="inputBubble"
                 type="text"
                 id="unamer"
                 name="unamer"
@@ -63,10 +63,10 @@ function RegisterPage({ onSwitchLoginClick }) {
           </div>
 
           {/* email bubble and input */}
-          <div className="w-[584px] h-[126px] left-[668px] top-[437px] relative">
+          <div className="left-[668px] top-[437px] inputContainer">
             <label className="w-full h-full" htmlFor="emlr">
               <input
-                className="rounded-[100px] border-2 border-[#aaaaaa] text-black text-2xl w-full h-full pl-10 placeholder:text-2xl placeholder:italic"
+                className="inputBubble"
                 type="email"
                 id="emlr"
                 name="emlr"
@@ -77,10 +77,10 @@ function RegisterPage({ onSwitchLoginClick }) {
           </div>
 
           {/* password bubble and input */}
-          <div className="w-[584px] h-[126px] left-[668px] top-[455px] relative">
+          <div className="left-[668px] top-[455px] inputContainer">
             <label className="w-full h-full" htmlFor="pwordr">
               <input
-                className="rounded-[100px] border-2 border-[#aaaaaa] text-black text-2xl w-full h-full pl-10 placeholder:text-2xl placeholder:italic"
+                className="inputBubble"
                 type="password"
                 id="pwordr"
                 name="pwordr"
@@ -92,13 +92,12 @@ function RegisterPage({ onSwitchLoginClick }) {
 
           {/* bee image */}
           <img
-            className="w-[126px] h-[138px] left-[898px] top-[271px] absolute"
             src={beeLogo}
             alt="site bee logo"
           />
 
           {/* Submit button */}
-          <div className="w-[237px] h-[102px] left-[860px] top-[455px] relative">
+          <div className="left-[860px] top-[455px] submitButtonContainer">
             <button
               type="submit"
               onClick={async (event) => {
@@ -141,14 +140,13 @@ function RegisterPage({ onSwitchLoginClick }) {
                 const json = await response.json();
                 alert(json.status);
               }}
-              className="w-[186px] h-[80px] top-[12px] relative bg-[#e5a000] text-black text-3xl font-semibold font-['Arial'] rounded-[100px] hover:text-white"
+              className="top-[12px] submitButton"
             >
               Register
             </button>
           </div>
         </form>
       </div>
-    </div>
   );
 }
 
